eab2fff9012f352eb0ba71c045c8e809b736ec5e
Abhinav Kumar Jha <akj0712@duck.com>
2022-09-26 23:51:36 +0530

    context api

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import noteContext from "../context/notes/noteContext";
 
 export const About = () => {
-  return (
-    <div>This is about</div>
-  )
-}
+  const a = useContext(noteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+
+  return <div>This is about {a.state.name}</div>;
+};
